d4dbe63d4e9fa5e63738daa1338c36b63e9b75e9
Adam Tuliper <adam.tuliper@gmail.com>
2017-10-10 00:16:24 -0700

    Added physics movement

---
+++ SimpleMovement/MoveWithPhysicsFromInput.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithPhysicsAndInput : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private float _horizontal;
+    private float _vertical;
+
+    public float Speed = 5;
+    public float RotationSpeed = 2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = transform.TransformDirection(((Vector3.forward * _vertical) + (Vector3.right * _horizontal)) * Speed);
+
+        //Let's get a left-right means rotation movement.
+        var euler = transform.localEulerAngles;
+        euler.y += _horizontal * RotationSpeed;
+
+        _rigidbody.rotation = Quaternion.Euler(euler);
+    }
+}
